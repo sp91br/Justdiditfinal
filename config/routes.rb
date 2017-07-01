9cfa747095fd688500c1c41c9f1ca1464158cb8a
Juan Pineda <jfpineda84@gmail.com>
2017-07-01 17:22:56 -0400

    new homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    root 'todo_items#index'
+    resources :todo_items
+    # a bunch of comments describing how to define routes
+  end
 end
